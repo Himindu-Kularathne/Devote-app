cdb884e435c9013d3edeef1df075971de8d323b6
Himindu <himindukularathne@gmail.com>
2024-01-09 13:23:25 +0530

    Initial Commit

---
+++ Devote-app/Devote_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Devote_appApp.swift
+//  Devote-app
+//
+//  Created by Himindu Kularathne on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct Devote_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
